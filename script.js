731b8553ad8914a9f31cc043f1189db54627c697
MelissaQ1999 <61278416+MelissaQ1999@users.noreply.github.com>
2021-02-28 23:35:57 +0100

    mobile version almost done!

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+// Script for the countdown clock
+// Set the date to where it counts down to
+var countDownDate = new Date("May 26, 2021 16:43:52").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -16,39 +17,44 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // Display the result in the element with id="countdown"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
+  // When the countdown is on the date, they shoul have arrived and this text will appear
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "Welcome to Mars";
   }
 }, 1000);
 
 
-// Script for bar chart of resources overview
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+// Sets the fonts of the charts
+Chart .defaults.global.defaultFontFamily = ' "GothamBold", Helvetica, sans-serif';
+
+// Script for graph of distance traveled over time
+var ctx = document.getElementById('distance').getContext('2d');
+var distance = new Chart(ctx, {
+    type: 'line',
     data: {
-        // This sets the data for the x-as
-        labels: ['Fuel', 'Water', 'Energy'],
+        labels: ['1 hr', '2 hr', '3 hr', '4 hr', '5 hr', '6 hr'],// This sets the data for the x-as
         datasets: [{
-            // label: '',
-            data: [12, 19, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
+            data: [85, 62, 77], // This determines the data displayed in the graph
+            backgroundColor: [ // This sets the fill color
+                'rgba(237, 83, 56, 0.3)',
+                'rgba(237, 83, 56, 0.3)',
+                'rgba(237, 83, 56, 0.3)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
+                'rgba(237, 83, 56, 1)',
+                'rgba(237, 83, 56, 1)',
+                'rgba(237, 83, 56, 1)'
             ],
-            borderWidth: 2
+            borderWidth: 5,
+            borderCapStyle: 'round',
+            // fill: false,
+            pointRadius: 5, // This determines the size of the graph point
+            pointHoverRadius: 10 // This will make the graph point bigger when you hover
         }]
     },
     options: {
@@ -58,6 +64,58 @@ var myChart = new Chart(ctx, {
                     beginAtZero: true
                 }
             }]
+        },
+        title: {
+            display: true,
+            text: 'Distance in km',
+        },
+        legend: {
+            display: false
+        }
+    }
+});
+
+
+// Script for bar chart of resources overview
+var ctx = document.getElementById('resources').getContext('2d');
+var resources = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Fuel', 'Water', 'Energy'],// This sets the data for the x-as
+        datasets: [{
+            data: [85, 62, 77], // This determines the data displayed in the bar chart
+            backgroundColor: [ // This sets the fill color
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 3,
+            hoverBackgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        title: {
+            display: true,
+            text: 'The resources are presented in %',
+        },
+        legend: {
+            display: false
         }
     }
 });
